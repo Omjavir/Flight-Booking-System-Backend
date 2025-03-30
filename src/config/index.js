d54da2aa9f3ed510b4dbf06f5b7a6f52c191f8b8
Omjavir <javirom07042003@gmail.com>
2025-03-30 10:38:35 +0530

    Add: added sequelize, mysql2 and sequelize-cli for db connnections and winston for logging

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,4 @@
-const dotenv = require("dotenv");
-
-dotenv.config();
-
 module.exports = {
-  PORT: process.env.PORT,
+  ServerConfig: require("./server-config"),
+  LoggerConfig: require("./logger-config"),
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { PORT } = require("./config");
+const { ServerConfig, LoggerConfig } = require("./config");
 const app = express();
 
 const apiRoutes = require("./routes");
 
 app.use("/api", apiRoutes);
 
-app.listen(PORT, () => {
-  console.log("App running on port", PORT);
+app.listen(ServerConfig.PORT, () => {
+  console.log("App running on port", ServerConfig.PORT);
+  LoggerConfig.info("successfully started Server", {});
 });
